8765a421f35c4d2487a6e0c23d681b2af79307f9
Eddy Kim <lasir3@gmail.com>
2022-03-12 21:34:46 +0900

    Java Instance example

---
+++ src/chap06/book/s061003/Television.java
@@ -0,0 +1,11 @@
+package chap06.book.s061003;
+
+public class Television {
+    static String company = "samsung";
+    static String model = "LCD";
+    static String info;
+
+    static {
+        info = company + "-" + model;
+    }
+}
