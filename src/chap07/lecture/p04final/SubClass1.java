c7866509bd466197c37954f76c76876562408851
Eddy Kim <lasir3@gmail.com>
2022-03-16 09:32:48 +0900

    Java promotion

---
+++ src/chap07/lecture/p04final/SubClass1.java
@@ -0,0 +1,14 @@
+package chap07.lecture.p04final;
+
+public class SubClass1 extends SuperClass1 {
+	@Override
+	public void method1() {
+		// TODO Auto-generated method stub
+		super.method1();
+	}
+	
+	// final 메소드는 재정의 불가
+//	public void method2() {
+//		
+//	} 
+}
---
+++ src/chap07/lecture/p04final/SubClass2.java
@@ -0,0 +1,5 @@
+package chap07.lecture.p04final;
+
+//public class SubClass2 extends SuperClass2 {
+//
+//} // final 클래스는 상속 불가
