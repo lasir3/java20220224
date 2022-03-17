8b3f289787ac627f4c231a2db1dbb4045a7bd085
Eddy Kim <lasir3@gmail.com>
2022-03-17 09:46:32 +0900

    Java interface

---
+++ src/chap08/lecture/p01interface/Myinterface2.java
@@ -0,0 +1,9 @@
+package chap08.lecture.p01interface;
+
+public interface Myinterface2 {
+	// 인터페이스에 작성되는 필드는
+	// 모두 public static final
+	int FIELD1 = 3;
+	public static final int FIELD2 = 9; // 자동으로 static final 이 붙는다
+	
+}
---
+++ src/chap08/lecture/p01interface/example_class/Chihuahua.java
@@ -0,0 +1,11 @@
+package chap08.lecture.p01interface.example_class;
+
+public class Chihuahua extends Canine implements Pet {
+
+	@Override
+	public void cry() {
+		System.out.println("멍멍!");
+		
+	}
+
+}
