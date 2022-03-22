96c1ebe2c7f37f3da79d47b7c1f60e4313fbe3c0
Eddy Kim <lasir3@gmail.com>
2022-03-22 18:15:57 +0900

    Java Annotation, Equals, HashCode

---
+++ src/chap11/book/example/s110601/ClassExample.java
@@ -0,0 +1,23 @@
+package chap11.book.example.s110601;
+
+import chap09.book.exercise.p04.Car;
+
+public class ClassExample {
+	public static void main(String[] args) {
+		Car car = new Car();
+		Class clazz1 = car.getClass();
+		System.out.println(clazz1.getName());
+		System.out.println(clazz1.getSimpleName());
+		System.out.println(clazz1.getPackage().getName());
+		System.out.println();
+		
+		try {
+			Class clazz2 = Class.forName("chap11.book.example.s110602.Car");
+			System.out.println(clazz2.getName());
+			System.out.println(clazz2.getSimpleName());
+			System.out.println(clazz2.getPackage().getName());
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		}
+	}
+}
