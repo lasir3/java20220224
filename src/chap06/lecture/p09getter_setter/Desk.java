f6754190707c75e105c50e756e7175f72af724cc
Eddy Kim <lasir3@gmail.com>
2022-03-30 03:34:10 +0900

    Java package access getter_setter

---
+++ src/chap06/lecture/p09getter_setter/Desk.java
@@ -0,0 +1,35 @@
+package chap06.lecture.p09getter_setter;
+
+public class Desk {
+	String model;
+	int height;
+	int weight;
+	int price;
+	
+	public String getModel() {
+		return model;
+	}
+	public void setModel(String model) {
+		this.model = model;
+	}
+	public int getHeight() {
+		return height;
+	}
+	public void setHeight(int height) {
+		this.height = height;
+	}
+	public int getWeight() {
+		return weight;
+	}
+	public void setWeight(int weight) {
+		this.weight = weight;
+	}
+	public int getPrice() {
+		return price;
+	}
+	public void setPrice(int price) {
+		this.price = price;
+	}
+	
+
+}
